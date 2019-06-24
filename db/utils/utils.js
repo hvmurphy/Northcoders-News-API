5f0609f0321210307494ee6562faf625173ba84d
hvmurphy <hvfmurphy@gmail.com>
2019-06-24 16:40:27 +0100

    created formatDate function

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,14 @@
-exports.formatDate = list => {};
+exports.formatDate = list => {
+  const newList = list.reduce((arr, element) => {
+    arr.push({ ...element, created_at: new Date(element["created_at"]) });
+    return arr;
+  }, []);
+  return newList;
+};
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = list => {
+  //list = [{article_id:1, title:A},{aticle_id:2, title:B}]
+  // tor return {A:1, B:2}
+};
 
 exports.formatComments = (comments, articleRef) => {};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,8 +1,116 @@
-const { expect } = require('chai');
-const { formatDate, makeRefObj, formatComments } = require('../db/utils/utils');
+const { expect } = require("chai");
+const { formatDate, makeRefObj, formatComments } = require("../db/utils/utils");
 
-describe('formatDate', () => {});
+describe.only("formatDate", () => {
+  it("returns a new array", () => {
+    const testList = [
+      {
+        article_id: 1,
+        title: "A",
+        body: "hey",
+        votes: 0,
+        topic: "topic1",
+        created_at: Date.now()
+      }
+    ];
+    const returnValue = formatDate(testList);
+    expect(testList).to.not.equal(returnValue);
+  });
+  it("does not mutate the input array", () => {
+    const testList = [
+      {
+        article_id: 1,
+        title: "A",
+        body: "hey",
+        votes: 0,
+        topic: "topic1",
+        created_at: 1561389315280
+      }
+    ];
+    formatDate(testList);
+    expect(testList).to.eql([
+      {
+        article_id: 1,
+        title: "A",
+        body: "hey",
+        votes: 0,
+        topic: "topic1",
+        created_at: 1561389315280
+      }
+    ]);
+  });
+  it("returns empty array when provided an empty array", () => {
+    expect(formatDate([])).to.eql([]);
+  });
+  it("changes date for a one object array", () => {
+    const testList = [
+      {
+        article_id: 1,
+        title: "A",
+        body: "hey",
+        votes: 0,
+        topic: "topic1",
+        created_at: Date.now()
+      }
+    ];
+    const result = [
+      {
+        article_id: 1,
+        title: "A",
+        body: "hey",
+        votes: 0,
+        topic: "topic1",
+        created_at: new Date()
+      }
+    ];
+    expect(formatDate(testList)).to.eql(result);
+  });
+  it("changes date for a multi object array", () => {
+    const testList = [
+      {
+        article_id: 1,
+        title: "A",
+        body: "hey",
+        votes: 0,
+        topic: "topic1",
+        created_at: Date.now()
+      },
+      {
+        article_id: 2,
+        title: "B",
+        body: "bye",
+        votes: 0,
+        topic: "topic2",
+        created_at: Date.now()
+      }
+    ];
+    const result = [
+      {
+        article_id: 1,
+        title: "A",
+        body: "hey",
+        votes: 0,
+        topic: "topic1",
+        created_at: new Date()
+      },
+      {
+        article_id: 2,
+        title: "B",
+        body: "bye",
+        votes: 0,
+        topic: "topic2",
+        created_at: new Date()
+      }
+    ];
 
-describe('makeRefObj', () => {});
+    expect(formatDate(testList)).to.eql(result);
+  });
+});
 
-describe('formatComments', () => {});
+describe("makeRefObj", () => {
+  it("", () => {});
+});
+
+describe("formatComments", () => {
+  it("", () => {});
+});
