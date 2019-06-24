5dde60c0dbec598bb31c5822836aeebaf0e9be06
hvmurphy <hvfmurphy@gmail.com>
2019-06-24 14:17:05 +0100

    created all tables

---
+++ db/migrations/20190624125440_create_articles_table.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+  console.log("creating articles table ...");
+  return knex.schema.createTable("articles", articlesTable => {
+    articlesTable.increments("article_id").primary();
+    articlesTable.string("title").notNullable();
+    articlesTable.string("body").notNullable();
+    articlesTable.integer("votes").defaultTo(0);
+    articlesTable.string("topic").references("topics.slug");
+    articlesTable.string("author").references("users.username");
+    articlesTable.timestamp("created_at");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  console.log("removing articles table ...");
+  return knex.schema.dropTable("articles");
+};
