2b926365d52d320406d8a3612051a7bfa7a654ba
hvmurphy <hvfmurphy@gmail.com>
2019-06-30 20:16:45 +0100

    fixed readfile path

--- models/api.js
+++ models/api.js
@@ -1,14 +1,10 @@
-const connection = require("../db/connection");
 const fs = require("fs");
 
 exports.fetchEndpoints = () => {
   return new Promise(function(resolve, reject) {
-    fs.readFile(
-      "/home/holly/Desktop/Northcoders/be-nc-news/endpoints.json",
-      (err, endpoints) => {
-        const parsedEndpoints = JSON.parse(endpoints);
-        err ? reject(err) : resolve(parsedEndpoints);
-      }
-    );
+    fs.readFile("./endpoints.json", (err, endpoints) => {
+      const parsedEndpoints = JSON.parse(endpoints);
+      err ? reject(err) : resolve(parsedEndpoints);
+    });
   });
 };
